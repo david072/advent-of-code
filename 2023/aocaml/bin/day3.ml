9f78e4855b028c2117f4e413b69dd2944300bf0b
david072 <david.g.ganz@gmail.com>
2023-12-03 10:14:48 +0100

    2023: Day 3, part 2
    This code is incredibly ugly, but it works so fine for now ig :shrug:

--- 2023/aocaml/bin/day3.ml
+++ 2023/aocaml/bin/day3.ml
@@ -15,10 +15,14 @@ let new_number x y chars =
     x = x;
     y = y;
     length = length;
-    number = Core.String.of_char_list chars |> int_of_string
+    number = Core.String.of_char_list chars |> int_of_string;
   }
 
-let parse_map input =
+type symbol = {
+  adjacent_numbers: (int * int) list;
+}
+
+let parse_map input ~include_char =
   let rec parse_lines sym_tbl nums y = function
     | [] -> (sym_tbl, nums)
     | line :: rest -> 
@@ -37,8 +41,11 @@ let parse_map input =
             else nums
             in
 
-            if char <> '.' then
-              Hashtbl.add sym_tbl (x, y) ();
+            if match include_char with
+                | None -> char <> '.'
+                | Some c -> char = c
+            then
+              Hashtbl.add sym_tbl (x, y) {adjacent_numbers = []};
 
             parse_line sym_tbl nums [] y (x + 1) rest
       in
@@ -48,7 +55,7 @@ let parse_map input =
   parse_lines (Hashtbl.create 1234) [] 0 @@ List.map string_to_char_list input
 
 let solve1 input = 
-  let (sym_tbl, nums) = parse_map input in
+  let (sym_tbl, nums) = parse_map input ~include_char:None in
   let rec search sym_tbl = function
     | [] -> 0
     | {x; y; length; number} :: rest -> 
@@ -71,4 +78,33 @@ let solve1 input =
   in
   search sym_tbl nums
 
-let solve2 _ = 0
+let solve2 input =
+  let (sym_tbl, nums) = parse_map input ~include_char:(Some '*') in
+  let rec mark sym_tbl = function
+    | [] -> ();
+    | {x; y; length; _} :: rest ->
+      let rec mark_vert sym_tbl x y ymax num_x num_y =
+        if y < ymax then begin
+          match Hashtbl.find_opt sym_tbl (x, y) with
+            | Some {adjacent_numbers} -> Hashtbl.add sym_tbl (x, y) @@ {adjacent_numbers = (num_x, num_y) :: adjacent_numbers};
+            | _ -> ();
+          mark_vert sym_tbl x (y + 1) ymax num_x num_y;
+        end
+      in
+      let rec mark_horz sym_tbl x y xmax num_x num_y =
+        if x < xmax then begin
+          mark_vert sym_tbl x (y - 1) (y + 2) num_x num_y;
+          mark_horz sym_tbl (x + 1) y xmax num_x num_y;
+        end
+      in
+      mark_horz sym_tbl (x - 1) y (x + length + 1) x y;
+      mark sym_tbl rest;
+  in
+  mark sym_tbl nums;
+  Hashtbl.filter_map_inplace (fun _ v -> if (List.length v.adjacent_numbers) = 2 then Some v else None) sym_tbl;
+  let gear_adj_nums = Hashtbl.fold (fun _ v acc -> v.adjacent_numbers :: acc) sym_tbl [] in
+  List.map (fun adj_nums ->
+    let nums = List.map (fun (x, y) -> List.find (fun num -> x = num.x && y = num.y) nums) adj_nums in
+    List.fold_left (fun acc num -> num.number * acc) 1 nums
+  ) gear_adj_nums
+  |> List.fold_left (fun ratio acc -> acc + ratio) 0
